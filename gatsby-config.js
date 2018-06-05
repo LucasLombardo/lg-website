252420718e7b8e8a5f2981c67ffd09e968ea0f13
Lucas Lombardo <Lucasl@bu.edu>
2018-06-05 18:11:37 -0400

    add markdown query and format index

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,25 @@ module.exports = {
     title: 'Gatsby Default Starter',
     desc: "A new blog",
   },
-  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-styled-components'],
+  plugins: [
+    'gatsby-plugin-react-helmet', 
+    'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'src',
+        path: `${__dirname}/src/`,
+      }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'img',
+        path: `${__dirname}/src/images`,
+      }
+    },
+    'gatsby-transformer-sharp', 
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-remark'
+  ],
 }
---
+++ src/pages/post.md
@@ -0,0 +1,12 @@
+---
+title: "Welcome to the new Blog Post"
+date: "2018-06-05"
+---
+
+## Hello
+
+* This
+* Is
+* A
+* New
+* Post
