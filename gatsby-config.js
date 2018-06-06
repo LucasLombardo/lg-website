9e36871efe3a052e5df5e6153203e2d449fbf83c
Lucas Lombardo <Lucasl@bu.edu>
2018-06-06 13:04:26 -0400

    change path

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: 'Gatsby Default Starter',
     desc: 'A new blog',
   },
-  pathPrefix: '/gatsby-blog',
+  pathPrefix: '/blog',
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
