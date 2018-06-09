da8f5958abf4a36c138ea6fc02243eeeb5861058
Lucas Lombardo <Lucasl@bu.edu>
2018-06-08 21:46:23 -0400

    remove blog related content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,9 @@
 import React from 'react';
-// import Link from 'gatsby-link';
-import PostListing from '../components/postListing';
 
 const IndexPage = ({ data }) => (
   <div>
     <h2>Posts</h2>
     <hr />
-    {data.allMarkdownRemark.edges.map(({ node }) => (
-      <PostListing key={node.id} post={node} />
-    ))}
   </div>
 );
 
@@ -21,24 +16,6 @@ export const query = graphql`
       siteMetadata {
         title
         desc
-      }
-    }
-    allMarkdownRemark(sort: {
-      fields: [frontmatter___date],
-      order: DESC
-    }) {
-      edges {
-        node {
-          id
-          frontmatter{
-            title
-            date(formatString: "MMMM DD, YYYY")
-          }
-          fields{
-            slug
-          }
-          excerpt(pruneLength: 60)
-        }
       }
     }
   }
