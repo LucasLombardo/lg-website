ac437d39aa3e502924b9e4a54950d5252765f3ed
Lucas Lombardo <Lucasl@bu.edu>
2018-06-20 19:20:47 -0400

    adjust netlify tag

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Contact = () => (
   <div>
-    <form name="contact" method="POST" netlify>
+    <form name="contact" method="POST" data-netlify="true" netlify>
       <p>
         <label>Your Name: <input type="text" name="name" /></label>
       </p>
