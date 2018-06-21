ab0c7e2526c716940b20db29634f8b47a7691612
Lucas Lombardo <Lucasl@bu.edu>
2018-06-21 06:40:42 -0400

    remove seperate success page

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,11 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledFooter = styled.div`
-    text-align: center;
-    font-size: .8em;
-    padding: 1em 0;
-    margin-top: 1.6em;
-    background-color: rgba(178, 206, 215, .3);
+  text-align: center;
+  font-size: .8em;
+  padding: 1em 0;
+  margin-top: 1.6em;
+  background-color: rgba(178, 206, 215, .3);
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
 `;
 
 const Footer = ({ title }) => (
