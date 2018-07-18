c3cf3d1fce09e33281a75d05e26e38b0d18c3686
Lucas Lombardo <Lucasl@bu.edu>
2018-07-18 09:08:32 -0400

    update description and readme

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: 'Lombardo Graphics',
-    desc: 'Vehicle Graphics and Sign company located in North Andover, Ma. Service offerings include Automobile and Truck lettering, Pinstriping, Airbrush work, Full Vehicle Wraps, and Custom Motorcycle Art.',
+    desc: 'Vehicle graphics and lettering company located in North Andover, Ma. Service offerings include Automobile and Truck lettering, Pinstriping, Airbrush work, Full Vehicle Wraps, and Custom Motorcycle Art.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
