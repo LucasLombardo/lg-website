42440764074dfb28f45920f302bc1629d86149b9
Lucas Lombardo <lucasalombardo@gmail.com>
2022-01-16 17:27:09 -0500

    adapts header animation to work with gatsby 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,9 @@ module.exports = {
     desc: 'Vehicle graphics and lettering company located in North Andover, Ma. Service offerings include Automobile and Truck lettering, Pinstriping, Airbrush work, Full Vehicle Wraps, and Custom Motorcycle Art.',
   },
   plugins: [
-    `gatsby-plugin-layout`
+    `gatsby-plugin-layout`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
   ],
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,11 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
+import React, { useLayoutEffect, useRef } from 'react'
 import styled from 'styled-components'
-// import Img from 'gatsby-image';
+import { StaticImage } from 'gatsby-plugin-image'
 import NavBar from '../components/navbar'
 
 const LogoWrapper = styled.div`
   width: 100%;
-  background-image: url(https://imagizer.imageshack.us/a/img921/3903/BaMl4X.jpg);
+  background-image: url('https://imagizer.imageshack.us/a/img921/3903/BaMl4X.jpg');
   background-repeat: repeat-x;
   background-position: center;
   background-color: #666;
@@ -20,6 +19,7 @@ const LogoWrapper = styled.div`
     img {
       max-width: 518px;
     }
+  }
   @media only screen and (max-width: 768px) {
     height: 150px;
     img {
@@ -34,6 +34,7 @@ const ImgWrapper = styled.div`
   @media only screen and (max-width: 1024px) {
     height: 225px;
     width: 518px;
+  }
   @media only screen and (max-width: 768px) {
     height: 150px;
     width: 345px;
@@ -54,62 +55,71 @@ function calcHeaderHeight(n) {
   return '300px'
 }
 
-/* export default class Header extends Component {
-  componentDidUpdate = (prevProps) => {
-    const { location } = this.props;
-    if (location.pathname !== prevProps.location.pathname) {
-      const vhwidth = (window.innerWidth) ? window.innerWidth : 2000;
-      const tallheight = calcHeaderHeight(vhwidth);
-      const shortheight = '0px';
-      if (this.props.location.pathname === '/') {
-        // if swiching to home page, animate the hieght to 500px
-        this.wrapper.animate([
+const Header = ({ location }) => {
+  const isHome = location.pathname === '/'
+  const wrapper = useRef(null)
+  const firstUpdate = useRef(true)
+
+  useLayoutEffect(() => {
+    if (firstUpdate.current) {
+      firstUpdate.current = false
+      return
+    }
+
+    const vhwidth = window.innerWidth ? window.innerWidth : 2000
+    const tallheight = calcHeaderHeight(vhwidth)
+    const shortheight = '0px'
+    if (isHome) {
+      // if swiching to home page, animate the hieght to 500px
+      wrapper.current.animate(
+        [
           // can set as many frames as you would like, we are just using 2 to animate the vh change
           { height: shortheight },
           { height: tallheight },
-        ], {
+        ],
+        {
           // duration is time of animation, fill forwards means it will stay on last frame
           duration: 300,
           fill: 'forwards',
           easing: 'cubic-bezier(0.86, 0, 0.07, 1)',
           iterations: 1,
-        });
-      } else if (prevProps.location.pathname === '/') {
-        // if swiching to another page from the home page, animate the hieght to 50px
-        this.wrapper.animate([
-          { height: tallheight },
-          { height: shortheight },
-        ], {
+        }
+      )
+    } else {
+      // if swiching to another page from the home page, animate the hieght to 50px
+      wrapper.current.animate(
+        [{ height: tallheight }, { height: shortheight }],
+        {
           duration: 300,
           fill: 'forwards',
           easing: 'cubic-bezier(0.86, 0, 0.07, 1)',
           iterations: 1,
-        });
-      }
+        }
+      )
     }
-  } */
+  }, [isHome])
 
-const Header = () => {
-  /* const { data, location } = this.props; */
   return (
     <div>
-      {/**         <LogoWrapper ref={wrapper => this.wrapper = ReactDOM.findDOMNode(wrapper)} isHome={location.pathname === '/'}>
-            {location.pathname === '/' && 
-              <ImgWrapper>
-                <div>
-                  <Img
-                    style={{
-                    position: 'relative',
-                    width: '100%',
-                    height: '100%',
-                    opacity: 0.85,
-                  }}
-                    sizes={data.logo.sizes}
-                  />
-                </div>
-              </ImgWrapper>
-          }
-        </LogoWrapper>**/}
+      <LogoWrapper ref={wrapper} isHome={isHome}>
+        {isHome && (
+          <ImgWrapper>
+            <div>
+              <StaticImage
+                placeholder="none"
+                src="../images/md_logo.png"
+                alt="Lombardo Graphics"
+                style={{
+                  position: 'relative',
+                  width: '100%',
+                  height: '100%',
+                  opacity: 0.85,
+                }}
+              />
+            </div>
+          </ImgWrapper>
+        )}
+      </LogoWrapper>
       <NavBar />
     </div>
   )
